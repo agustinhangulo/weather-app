b1e49f5c43306acfa18a0ecaf1784a4dde3a81a3
agustinhangulo <99315183+agustinhangulo@users.noreply.github.com>
2023-07-12 14:19:40 -0700

    Initial commit

---
+++ src/model.js
@@ -0,0 +1,14 @@
+/**
+ * This model will control the functionality for our weather app (requesting
+ * weather information through WeatherAPI, sending data to controller, etc.)
+ */
+class Model {
+  /**
+   * Bruh
+   */
+  constructor() {
+
+  }
+}
+
+export {Model};
